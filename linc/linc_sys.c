bfb44a200b7f6e72f77d4401de6fc1e978874bc3
Lars Brinkhoff <lars@nocrew.org>
2025-10-26 16:02:17 +0100

    LINC: Fixes for disassembler.

--- linc/linc_sys.c
+++ linc/linc_sys.c
@@ -224,7 +224,9 @@ static t_stat fprint_index(FILE *of, uint16 insn, uint16 addr)
 static void fprint_set(FILE *of, uint16 insn, uint16 addr)
 {
   fprintf(of, "SET");
-  fprint_index(of, insn, addr);
+  if (insn & IMASK)
+    fprintf(of, " i");
+  fprintf(of, " %o", insn & BMASK);
   fprint_next(of, addr);
 }
 
@@ -535,7 +537,7 @@ t_stat fprint_sym(FILE *of, t_addr addr, t_value *val, UNIT *uptr, int32 sw)
   case 00700:
   case 00740:
     fprint_tape(of, *val, addr);
-    break;
+    return -1;
   case 01000:
     return fprint_lda(of, *val, addr);
   case 01040:
